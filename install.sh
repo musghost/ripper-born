06c341047ce956cb052b38fa3cf6f55a5e05d79b
Ricardo Alberto Pérez Candelas <ricardo.pcan@gmail.com>
2017-02-22 23:45:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 sudo pacman -Syu
 
-sudo pacman -S --noconfirm chromium
-sudo pacman -S --noconfirm virtualbox
-sudo pacman -S --noconfirm gnome-terminal
-sudo pacman -S --noconfirm vagrant
-sudo pacman -S --noconfirm tmux
-sudo pacman -S --noconfirm zsh
-sudo pacman -S --noconfirm docker
-sudo gpasswd -a $USER docker
-sudo systemctl start docker
-sudo pacman -S --noconfirm rofi
-yaourt -S --noconfirm spotify
-yaourt -S --noconfirm gpmdp
+#sudo pacman -S --noconfirm chromium
+#sudo pacman -S --noconfirm virtualbox
+#sudo pacman -S --noconfirm gnome-terminal
+#sudo pacman -S --noconfirm vagrant
+#sudo pacman -S --noconfirm tmux
+#sudo pacman -S --noconfirm zsh
+#sudo pacman -S --noconfirm docker
+#sudo gpasswd -a $USER docker
+#sudo systemctl start docker
+#sudo pacman -S --noconfirm rofi
+#yaourt -S --noconfirm spotify
+#yaourt -S --noconfirm gpmdp
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#\curl -sSL https://get.rvm.io | bash -s stable
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+git clone https://github.com/ricardo-pcan/ripper.git ~/.ripper
